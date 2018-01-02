64caa7bffdba2e0339f50d5f31afab9ac0dcb2a8
steven0129 <h3584935@gmail.com>
2018-01-03 01:30:36 +0800

    Add Onehot encoder into this project and refactor all codes

--- s2s.py
+++ s2s.py
@@ -1,16 +1,26 @@
 from keras.models import Sequential
-from keras.layers.recurrent import LSTM, GRU
-from keras.layers.wrappers import TimeDistributed, Bidirectional
-from keras.layers.core import Dense, RepeatVector
+from keras.layers import LSTM, Embedding
+from keras import layers
 
 
-def model1(input_dim, hidden_dim, max_in_seq_len=50, max_out_seq_len=1000):
+def model1(INPUT_MAX_LEN, WORD_DIM, OUTPUT_MAX_LEN, HIDDEN_DIM, DEPTH=1):
     model = Sequential()
-    model.add(Bidirectional(GRU(input_dim=input_dim,
-                                output_dim=hidden_dim), input_shape=(max_in_seq_len, input_dim)))
-    model.add(Dense(hidden_dim, activation='softmax'))
-    model.add(RepeatVector(max_out_seq_len))
-    model.add(Bidirectional(GRU(hidden_dim, return_sequences=True)))
-    model.add(TimeDistributed(Dense(output_dim=input_dim, activation='softmax')))
-
+    model.add(Embedding(WORD_DIM, HIDDEN_DIM, input_length=INPUT_MAX_LEN))
+    # "Encode" the input sequence using an RNN, producing an output of HIDDEN_DIM.
+    # Note: In a situation where your input sequences have a variable length,
+    # use input_shape=(None, num_feature).
+    model.add(LSTM(HIDDEN_DIM))
+    # As the decoder RNN's input, repeatedly provide with the last hidden state of
+    # RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
+    # length of output, e.g., when DIGITS=3, max output is 999+999=1998.
+    model.add(layers.RepeatVector(OUTPUT_MAX_LEN))
+    # The decoder RNN could be multiple layers stacked or a single layer.
+    for _ in range(DEPTH):
+        # By setting return_sequences to True, return not only the last output but
+        # all the outputs so far in the form of (num_samples, timesteps,
+        # output_dim). This is necessary as TimeDistributed in the below expects
+        # the first dimension to be the timesteps.
+        model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+    model.add(layers.TimeDistributed(layers.Dense(WORD_DIM)))
+    model.add(layers.Activation('softmax'))
     return model
--- train.py
+++ train.py
@@ -2,55 +2,39 @@ import s2s
 import mystr
 import myfile
 import numpy as np
+import pickle
+from tqdm import *
+from numpy import array
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
 
-MAX_INPUT=50
-MAX_OUTPUT=1000
+MAX_INPUT = 70
+MAX_OUTPUT = 20000
+WORD_DIM = 195102
 
-csvArr=myfile.readcsv('./datasets/news20171225.csv')
+csvArr = myfile.readcsv('./datasets/news20171225.csv')
 
-inputArr=[]
-outputArr=[]
+inputArr = []
+outputArr = []
+X = np.array([]).reshape(-1, MAX_INPUT)
+Y = np.array([]).reshape(-1, MAX_OUTPUT, WORD_DIM)
+
 for i in range(0, len(csvArr)):
     inputArr.append(csvArr[i][0])
     outputArr.append(csvArr[i][2])
 
-X = []
-Y = []
+with open('label.pickle', 'rb') as f:
+    encoder = pickle.load(f)
 
-for i in range(0, len(inputArr)):
-    input = inputArr[i]
-    output = outputArr[i]
-    tempIn = []
-    tempOut = []
-
-    for j in range(0, MAX_INPUT):
-        try:
-            unicode10 = mystr.decode(input[j])
-            unicode2 = mystr.int2bin(unicode10)
-            oneHot = mystr.str2arr(unicode2)
-            tempIn.append(oneHot)
-        except IndexError:
-            tempIn.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    for i in range(0, len(inputArr)):
+        print()
+        raw = array(encoder.transform(list(inputArr[i])))
+        fill = np.append(raw, np.zeros(MAX_INPUT - raw.size)).reshape(-1, MAX_INPUT)
+        X = np.append(X, fill, axis=0)
         
-
-    for j in range(0, MAX_OUTPUT):
-        try:
-            unicode10 = mystr.decode(output[j])
-            unicode2 = mystr.int2bin(unicode10)
-            oneHot = mystr.str2arr(unicode2)
-            tempOut.append(oneHot)
-        except IndexError:
-            tempOut.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        
-
-    X.append(tempIn)
-    Y.append(tempOut)
-
-Xnp = np.array(X)
-Ynp = np.array(Y)
-
-model = s2s.model1(input_dim=16, hidden_dim=32)
-model.summary()
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam', metrics=['acc'])
-model.fit(Xnp, Ynp, epochs=50)
+        raw = array(encoder.transform(list(inputArr[i])))
+        fill = np.append(raw, np.zeros(MAX_OUTPUT - raw.size)).reshape(-1, MAX_OUTPUT)
+        with open('onehot.pickle', 'rb') as f2:
+            onehot = pickle.load(f2)
+            val=onehot.transform(fill.reshape(fill.size, 1)) # csr_matrix

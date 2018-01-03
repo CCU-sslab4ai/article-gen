e15053344c0d037ee31c3423ca5d2d6e792f4429
steven0129 <h3584935@gmail.com>
2018-01-03 18:26:05 +0800

    Prototype complete

--- s2s.py
+++ s2s.py
@@ -1,25 +1,15 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Embedding
+from keras.layers.core import Reshape
 from keras import layers
 
 
 def model1(INPUT_MAX_LEN, WORD_DIM, OUTPUT_MAX_LEN, HIDDEN_DIM, DEPTH=1):
     model = Sequential()
     model.add(Embedding(WORD_DIM, HIDDEN_DIM, input_length=INPUT_MAX_LEN))
-    # "Encode" the input sequence using an RNN, producing an output of HIDDEN_DIM.
-    # Note: In a situation where your input sequences have a variable length,
-    # use input_shape=(None, num_feature).
     model.add(LSTM(HIDDEN_DIM))
-    # As the decoder RNN's input, repeatedly provide with the last hidden state of
-    # RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
-    # length of output, e.g., when DIGITS=3, max output is 999+999=1998.
     model.add(layers.RepeatVector(OUTPUT_MAX_LEN))
-    # The decoder RNN could be multiple layers stacked or a single layer.
     for _ in range(DEPTH):
-        # By setting return_sequences to True, return not only the last output but
-        # all the outputs so far in the form of (num_samples, timesteps,
-        # output_dim). This is necessary as TimeDistributed in the below expects
-        # the first dimension to be the timesteps.
         model.add(LSTM(HIDDEN_DIM, return_sequences=True))
     model.add(layers.TimeDistributed(layers.Dense(WORD_DIM)))
     model.add(layers.Activation('softmax'))
--- train.py
+++ train.py
@@ -8,10 +8,11 @@ from numpy import array
 from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
 from keras.layers import Embedding, Flatten, Dense
+from keras.callbacks import Callback, ProgbarLogger, BaseLogger
 
 MAX_INPUT = 70
 MAX_OUTPUT = 20000
-WORD_DIM = 195102
+WORD_DIM = 21574
 
 csvArr = myfile.readcsv('./datasets/news20171225.csv')
 
@@ -19,7 +20,7 @@ inputArr = []
 outputArr = []
 X = np.array([]).reshape(-1, MAX_INPUT)
 
-Ymatrix=[]
+Ymatrix = []
 Y = np.array([]).reshape(-1, MAX_OUTPUT, WORD_DIM)
 
 print('讀取csv')
@@ -27,20 +28,37 @@ for i in trange(0, len(csvArr)):
     inputArr.append(csvArr[i][0])
     outputArr.append(csvArr[i][2])
 
-f= open('label.pickle', 'rb')
+f = open('label.pickle', 'rb')
 encoder = pickle.load(f)
 
-print('將X存放至Numpy, 將Y存放至csv_matrix')
+print('將X存放至Numpy, 將Y存放至csr_matrix')
 for i in trange(0, len(inputArr)):
     raw = array(encoder.transform(list(inputArr[i])))
-    fill = np.append(raw, np.zeros(MAX_INPUT - raw.size)).reshape(-1, MAX_INPUT)
+    fill = np.append(raw, np.zeros(MAX_INPUT - raw.size)
+                     ).reshape(-1, MAX_INPUT)
     X = np.append(X, fill, axis=0)
-    
+
     raw = array(encoder.transform(list(inputArr[i])))
-    fill = np.append(raw, np.zeros(MAX_OUTPUT - raw.size)).reshape(-1, MAX_OUTPUT)
+    fill = np.append(raw, np.zeros(MAX_OUTPUT - raw.size)
+                     ).reshape(-1, MAX_OUTPUT)
+
     with open('onehot.pickle', 'rb') as f2:
         onehot = pickle.load(f2)
         val = onehot.transform(fill.reshape(fill.size, 1))  # csr_matrix
         Ymatrix.append(val)
 
 print(len(Ymatrix))
+
+model = s2s.model1(MAX_INPUT, WORD_DIM, MAX_OUTPUT, 64)
+model.summary()
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam', metrics=['acc'])
+
+
+def generator(Xnp, YSparse):
+    print('開始Training...')
+    for i in trange(0, len(YSparse)):
+        yield Xnp[i].reshape(-1, MAX_INPUT), YSparse[i].toarray().reshape((-1, MAX_OUTPUT, WORD_DIM))
+
+
+model.fit_generator(generator(X, Ymatrix), steps_per_epoch=len(Ymatrix), epochs=1)
